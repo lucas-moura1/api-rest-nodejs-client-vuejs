e5f7c31410b74c4071ea5238faf74b9acd1e3065
Lucas Moura <lucas3ms.ls@gmail.com>
2021-03-30 09:35:56 -0300

    Configuração do express

---
+++ src/app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+
+const app = express()
+
+const { PORT } = require('./config')
+const loggerMiddleware = require('./middleware/logger')
+const routes = require('./routes')
+
+const port = PORT || 9090
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(loggerMiddleware)
+app.use(routes)
+
+app.use((req, res) => {
+    res.status(404).json({ message: 'Rota não encontrada' });
+})
+
+app.use(function (err, req, res, next) {
+    return res.status(err.statusCode).json({ error: err.message })
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+})
