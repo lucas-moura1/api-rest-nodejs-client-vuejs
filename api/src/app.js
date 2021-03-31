e50da5cae5d75c5ca03bd7ca8d5d0b1a646bdebe
Lucas Moura <lucas3ms.ls@gmail.com>
2021-03-31 10:51:57 -0300

    Configuração do cors no express

--- api/src/app.js
+++ api/src/app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 
 const app = express()
 
@@ -8,6 +9,7 @@ const routes = require('./routes')
 
 const port = PORT || 9090
 
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(loggerMiddleware)
